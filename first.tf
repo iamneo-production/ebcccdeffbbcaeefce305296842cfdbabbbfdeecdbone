4522164cfbea74f90ec4ea015802a613c06c2886
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:17:40 +0000

    Backup Commit

--- first.tf
+++ first.tf
@@ -11,5 +11,12 @@ provider{
     secret_key=" "
 }
 resource "aws_instance" "linux_server"{
-    ami=
+    ami=" "
+    instance_type="t2.micro"
+    tags={
+        Name="MyLinuxServer"
+    }
+}
+output "public_ip"{
+    value=aws_instance.linux_server.public_ip
 }
